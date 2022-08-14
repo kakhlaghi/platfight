4f6500a6ea63707f9d5b210843330b4b4b2bd852
Kevin Akhlaghi <kakhlaghi2002@gmail.com>
2022-08-14 16:37:22 -0400

    Character Select working
    Changes animation controller and sprites.. problem: these sprites arent imported at center
    ONLY DOG WORKING
    I want to overhaul the graphics to the bandits anyway for basically a different game anyway so im not worried plus even if i didnt it's not a big change just extra work.

--- Assets/CharacterChoiceObj.cs
+++ Assets/CharacterChoiceObj.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 [System.Serializable]
 public class CharacterChoiceObj
 {
    public string characterName;
    public Sprite characterSprite;
+   public AnimatorController characterAnimationCtrl;
 
 }
--- Assets/PlayerSetter.cs
+++ Assets/PlayerSetter.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class PlayerSetter : MonoBehaviour
 {
     public CharacterDatabase characterDB;
     public SpriteRenderer artSprite;
+    public Animator animator;
 
     private int selectedOption = 0;
     // Start is called before the first frame update
     void Start()
     {
+        //animator = this.GetComponent<Animator>();
         if(!PlayerPrefs.HasKey("selectedOption")){
             selectedOption = 0;
         } else {
@@ -22,7 +25,11 @@ public class PlayerSetter : MonoBehaviour
     private void UpdateCharacter (int selectedOption){
         Debug.Log(selectedOption);
         CharacterChoiceObj character = characterDB.GetCharacter(selectedOption);
+        Debug.Log(character.characterName);
+        Debug.Log(character.characterAnimationCtrl);
+
         artSprite.sprite = character.characterSprite;
+        animator.runtimeAnimatorController = character.characterAnimationCtrl as RuntimeAnimatorController;
     }
 
 
